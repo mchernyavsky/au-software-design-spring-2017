7e1a85836346c22896309d2bdb533c52fa07e35f
mchernyavsky <chemike47@gmail.com>
2017-03-11 18:26:05 +0300

    Make parser methods private that don't need to be public

--- src/main/scala/ru/spbau/mit/softwaredesign/cli/parser/CommandLineParsers.scala
+++ src/main/scala/ru/spbau/mit/softwaredesign/cli/parser/CommandLineParsers.scala
@@ -8,41 +8,33 @@ object CommandLineParsers extends RegexParsers {
   private val assignmentOperator = "="
   private val substitutionOperator = "$"
 
-  /** A parser that matches assignment or application */
-  def expression: Parser[Expression] = assignment | application
+  private def expression: Parser[Expression] = assignment | application
 
-  /** A parser that matches composition */
-  def composition: Parser[Composition] = expression ~ opt(compositionOperator ~> composition) ^^ {
+  private def composition: Parser[Composition] = expression ~ opt(compositionOperator ~> composition) ^^ {
     case expr ~ compositionRest => Composition(expr, compositionRest.orNull)
   }
 
-  /** A parser that matches assignment */
-  def assignment: Parser[Assignment] = variable ~ (assignmentOperator ~> block) ^^ {
+  private def assignment: Parser[Assignment] = variable ~ (assignmentOperator ~> block) ^^ {
     case variable ~ block => Assignment(variable, block)
   }
 
-  /** A parser that matches application */
-  def application: Parser[Application] = block ~ rep(block) ^^ {
+  private def application: Parser[Application] = block ~ rep(block) ^^ {
     case function ~ args => Application(function, args)
   }
 
-  /** A parser that matches substitution or block */
-  def block: Parser[Block] = substitution ^^ { Block(_) } | unQuotedLiteral ^^ { Block(_) } |
-                             weakQuotedBlock | fullQuotedBlock
+  private def block: Parser[Block] = substitution ^^ { Block(_) } | unQuotedLiteral ^^ { Block(_) } |
+                                     weakQuotedBlock | fullQuotedBlock
 
-  /** A parser that matches substitution */
-  def substitution: Parser[Substitution] = substitutionOperator ~> variable ^^
+  private def substitution: Parser[Substitution] = substitutionOperator ~> variable ^^
     (variable => Substitution(variable))
 
-  /** Parsers that match literals */
-  def variable: Parser[Literal] = """[A-Za-z_]\w*""".r ^^ { Literal }
-  def unQuotedLiteral: Parser[Literal] = """[^'"$|\s]+""".r ^^ { Literal }
-  def weakQuotedLiteral: Parser[Literal] = """[^"$]+""".r ^^ { Literal }
-  def fullQuotedLiteral: Parser[Literal] = """[^']+""".r ^^ { Literal }
+  private def variable: Parser[Literal] = """[A-Za-z_]\w*""".r ^^ { Literal }
+  private def unQuotedLiteral: Parser[Literal] = """[^'"$|\s]+""".r ^^ { Literal }
+  private def weakQuotedLiteral: Parser[Literal] = """[^"$]+""".r ^^ { Literal }
+  private def fullQuotedLiteral: Parser[Literal] = """[^']+""".r ^^ { Literal }
 
-  /** Parsers that match quoted block */
-  def weakQuotedBlock: Parser[Block] = "\"" ~> rep(substitution | weakQuotedLiteral) <~ "\"" ^^ { Block(_) }
-  def fullQuotedBlock: Parser[Block] = "'" ~> rep(fullQuotedLiteral) <~ "'" ^^ { Block(_) }
+  private def weakQuotedBlock: Parser[Block] = "\"" ~> rep(substitution | weakQuotedLiteral) <~ "\"" ^^ { Block(_) }
+  private def fullQuotedBlock: Parser[Block] = "'" ~> rep(fullQuotedLiteral) <~ "'" ^^ { Block(_) }
 
   /** Parse all of String `line` with parser `composition` */
   def parse(line: String): ParseResult[Composition] = parseAll(composition, line)
